c7b3d97e583b829239565a0e1e3f08b244f3d37f
Adrian <dmitrukadrian@gmail.com>
2024-02-26 15:29:10 +0300

    fix: error button check

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react"
-import clever from "../assets/clever.png"
-import inno from "../assets/inno.jpg"
+// import clever from "../assets/clever.png"
+// import inno from "../assets/inno.jpg"
 import { AboutMain, Contact, First, Projects, Skills } from "../components"
 const Home: FC = () => {
 	return (
 		<>
 			<First />
-			<div className='ser'>
+			{/* <div className='ser'>
 				<img className='ser_img' src={clever} alt='clever' />
 				<img className='ser_img' src={inno} alt='inno' />
-			</div>
+			</div> */}
 			<Projects />
 			<Skills />
 			<AboutMain />
